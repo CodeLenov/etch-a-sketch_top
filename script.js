cb2dba3e2d0b96d8b8f7ff05f0f1397057ff5a49
codelenov <codelenovo@tutanota.com>
2024-04-19 13:55:11 +0200

    Add functions for asking the number of squares and create grid

--- script.js
+++ script.js
@@ -1,11 +1,52 @@
-let container = document.querySelector("#container");
+let gridContainer = document.querySelector("#gridContainer");
+let gridSize = 16*16;
 
-for (let i=0; i < (16*16); i++) {
-	let items = document.createElement("div");
-	items.classList.add("items");
-	container.appendChild(items);
+function getGridSize() {
+	let gridForm = document.querySelector("#gridForm");
+
+	gridForm.addEventListener("submit", (e) => {
+		e.preventDefault();
+		let gridInput = document.querySelector("#gridInput");
+		let gridText = document.querySelector("#gridText");
+
+		if (gridInput.value < 1 || gridInput.value > 100) {
+			gridText.innerHTML = `Enter a number from 1 to 100`;
+		} else {
+			gridText.innerHTML = `This grid has ${gridInput.value}*${gridInput.value} squares`;
+			gridSize = gridInput.value * gridInput.value;
+			createGrid();
+		}
+
+	});
 }
 
-items.addEventListener("mouseover", () => {
-	items.style.backgroundColor = "blue";
+function createGrid() {
+
+	gridContainer.innerHTML = "";
+
+	for (let i=0; i < (gridSize); i++) {
+		let gridSquares = document.createElement("div");
+		gridSquares.classList.add("gridSquares");
+		gridSquares.style.cssText = `
+			width: calc(100% / ${gridInput.value});
+			height: calc(100% / ${gridInput.value});
+			background-color: #eee;
+			border: 1px solid #ccc;
+		`;
+		gridContainer.appendChild(gridSquares);
+		gridContainer.style.cssText = `
+			width: 100%;
+			display: flex;
+			flex-wrap: wrap;
+			aspect-ratio: 1/1;
+		`;
+	}
+
 }
+
+getGridSize();
+
+
+/*squares.addEventListener("mouseover", () => {
+	squares.style.backgroundColor = "blue";
+}*/
